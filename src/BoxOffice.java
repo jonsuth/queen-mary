912501b36ba0d5970213b9552a8bfa58642fa197
jonsuth <jonathan.suthakaran@gmail.com>
2016-10-09 23:41:05 +0100

    Done box office program

--- src/BoxOffice.java
+++ src/BoxOffice.java
@@ -1,9 +1,11 @@
 import java.util.*;
 /**
  * Created by Jon Suthakaran on 05/10/2016.
- *
+ * A program that simulates a simple box office
+ * Allows input for film showings and displays the showings at the end of the program
  */
 public class BoxOffice {
+
     public static void main(String[] args) {
         ArrayList<Film> films = new ArrayList<>();  // Array list holds all films in the box office
 
@@ -21,84 +23,88 @@ public class BoxOffice {
      * @param screen
      * @return films
      */
-    public static ArrayList<Film> populateFilms(ArrayList<Film> films, int screen) {
-        Scanner sc = new Scanner(System.in);
+    private static ArrayList<Film> populateFilms(ArrayList<Film> films, int screen) {
         String title;
-        int hour;
-        int minute;
         Film film;
+        Time time;
+            while (true) {  // Loop until all films have been created
 
-            System.out.println("Film for screen " + screen + "? ");
-            title = sc.nextLine();
-            while (true) {  // Loop until correct time is inputted
-                System.out.println("What time does it start? Hour (24 Hour clock - 0-23)");
-                hour = sc.nextInt();
+                title = readTitle(screen);
+                time = new Time(readHour(), readMinute()); // Create time input with readings from user
+                film = createFilm(title, time, screen);  // Try create film Returns null if film cant be created
 
-                System.out.println("What time does it start? Minutes after hour (0-60)");
-                minute = sc.nextInt();
-
-                film = createFilm(title, hour, minute, screen);  // Try create film Returns null if film cant be created
-
-                if (film != null) { // Break loop if film was created
+                if (film != null) { // Break loop if film was created else continue
                     break;
                 }
             }
 
         films.add(film);
-
         System.out.println();
         return films;  // Return the array list of films
     }
 
     /**
-     * Displays all films in the films array, pads title to fit 25 chars
+     * Get title name from user.
+     * @param screen
+     * @return title
+     */
+    private static String readTitle(int screen) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Film for screen " + screen + "?");
+        return sc.nextLine();
+    }
+
+    /**
+     * Get film starting hour from user.
+     * @return hour
+     */
+    private static int readHour() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What time does it start? Hour (24 Hour clock - 0-23)");
+        return sc.nextInt();
+    }
+
+    /**
+     * Get film starting minute from user.
+     * @return minute
+     */
+    private static int readMinute() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What time does it start? Minutes after hour (0-60)");
+        return sc.nextInt();
+    }
+
+    /**
+     * Displays all films in the films array, pads title to fit 25 chars.
      * @param films
      */
-    public static void showFilms(ArrayList<Film> films) {
+    private static void showFilms(ArrayList<Film> films) {
         System.out.println("CinemaWorld Films Tonight");
         for (Film film: films) {
             System.out.println("Screen " + film.getScreen()
                     + ": " + String.format("%1$-25s",film.getTitle())
-                    + " " + film.getHour()
-                    + ":" + film.getMinute());
+                    + " " + String.format("%02d", film.getTime().getHour())
+                    + ":" + String.format("%02d", film.getTime().getMinute()));
         }
     }
 
     /**
-     * Checks the fields of the film and creates a Film object
+     * Checks the fields of the film and creates a Film object.
+     * Returns null if film object cant be created (bad fields).
      * @param title
-     * @param hour
-     * @param minute
+     * @param time
      * @param screen
-     * @return film
+     * @return film/null
      */
-    public static Film createFilm(String title, int hour, int minute, int screen) {
-        Film film = new Film();
-        film.setScreen(screen);
-        film.setTitle(title);
+    private static Film createFilm(String title, Time time, int screen) {
 
-        if (errorCheckTime(hour, minute)) {  // If the minute and hours conform to the standards
-            if (minute == 60) {  // Rollover the hour if the minute is 60
-                if ((hour + 1) == 24) {  // Rollover the hour to midnight if it reaches 24
-                    hour = 0;
-                } else {
-                    hour++;
-                }
-                minute = 0;
-
-                // If the start time has changed due to rollovers let the user know
-                System.out.println("Start time for this film has changed to "
-                        + String.format("%02d", hour)
-                        + ":"
-                        + String.format("%02d", minute));
-            }
-            film.setHour(String.format("%02d", hour));
-            film.setMinute(String.format("%02d", minute));
+        if (checkTime(time.getHour(), time.getMinute())) {  // If the minute and hours conform to the standards
+            time = adjustTime(time);  // Adjust time for any rollovers
+            return new Film(title, time, screen);
         } else {
             System.out.println("Error with time, please try again");
             return null;
         }
-        return film;
     }
 
     /**
@@ -107,8 +113,33 @@ public class BoxOffice {
      * @param minute
      * @return true/false
      */
-    public static boolean errorCheckTime(int hour, int minute) {
+    private static boolean checkTime(int hour, int minute) {
         return ((hour >= 0 && hour <= 23) && (minute >= 0 && minute <= 60));
+    }
+
+    /**
+     * Rollover the hour and minute if needed else do nothing
+     * @param time
+     * @return time
+     */
+    private static Time adjustTime(Time time) {
+
+        if (time.getMinute() == 60) {  // Rollover the hour and minute if the minute is 60
+            if ((time.getHour() + 1) == 24) {  // Rollover the hour to midnight(00:--) if it reaches 24
+                time.rolloverHour();
+            } else {
+                time.incrementHour();
+            }
+            time.rolloverMinute();
+
+            // If the start time has changed due to rollovers let the user know
+            System.out.println("Start time for this film has changed to "
+                    + String.format("%02d", time.getHour())
+                    + ":"
+                    + String.format("%02d", time.getMinute()));
+        }
+
+        return time;
     }
 
 }
@@ -118,17 +149,39 @@ public class BoxOffice {
  */
 class Film {
     private String title;
-    private String hour;
-    private String minute;
+    private Time time;
     private int screen;
 
     String getTitle() { return title; }
-    String getHour() { return hour; }
-    String getMinute() { return minute; }
+    Time getTime() { return time; }
     int getScreen() { return screen; }
 
-    void setTitle(String title) { this.title = title; }
-    void setHour(String hour) { this.hour = hour; }
-    void setMinute(String minute) { this.minute = minute; }
-    void setScreen(int screen) { this.screen = screen; }
+    Film(String title, Time time, int screen) {
+        this.title = title;
+        this.time = time;
+        this.screen = screen;
+    }
+}
+
+/**
+ * Class that represents time, contains hour and minute
+ */
+class Time {
+    private int hour;
+    private int minute;
+
+    int getHour() { return hour; }
+
+    int getMinute() { return minute; }
+
+    void rolloverHour() { this.hour = 0; }
+
+    void rolloverMinute() { this.minute = 0; }
+
+    void incrementHour() { this.hour = this.hour + 1; }
+
+    Time(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
 }
